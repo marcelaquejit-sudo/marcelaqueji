2d6f4903fe514de4bd1c4ca986f73b6918684a29
Marcela Queji <marcelaqueji.t@gmail.com>
2025-11-02 11:19:02 -0300

    Add route for EEA component in App

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import LojaVirtualTB from "./components/LojaVirtualTB";// crie esse arquivo e cole a LP inteira
-
+import EEA from "./components/EEA";
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/loja-virtual-tb" element={<LojaVirtualTB />} />
+      <Route path="/eu-escolho-me-amar" element={<EEA />} />
     </Routes>
   );
 }
